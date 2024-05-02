abf2badd4e9987eb57b4a4b7bb639fe2830d72e8
Namitha-234 <139944475+Namitha-234@users.noreply.github.com>
2024-05-02 13:14:14 +0530

    Update CategoryProduct.jsx

--- src/pages/CategoryProduct.jsx
+++ src/pages/CategoryProduct.jsx
@@ -15,7 +15,7 @@ const CategoryProduct = () => {
   const getPrductsByCat = async () => {
     try {
       const { data } = await axios.get(
-        `/api/v1/product/product-category/${params.slug}`
+        `https://mern-ecommerce-g655.onrender.com/api/v1/product/product-category/${params.slug}`
       );
       setProducts(data?.products);
       setCategory(data?.category);
@@ -35,7 +35,7 @@ const CategoryProduct = () => {
               {products?.map((p) => (
                 <div className="card m-2" key={p._id}>
                   <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
+                    src={`https://mern-ecommerce-g655.onrender.com/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                   />
@@ -55,7 +55,7 @@ const CategoryProduct = () => {
                     <div className="card-name-price">
                       <button
                         className="btn btn-info ms-1"
-                        onClick={() => navigate(`/product/${p.slug}`)}
+                        onClick={() => navigate(`https://mern-ecommerce-g655.onrender.com/product/${p.slug}`)}
                       >
                         More Details
                       </button>
