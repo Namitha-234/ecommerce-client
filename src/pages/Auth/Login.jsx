86411cdb2b72cc2c11a10a6d86c069afd18bfeef
Namitha-234 <139944475+Namitha-234@users.noreply.github.com>
2024-05-02 13:24:05 +0530

    Update Login.jsx

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", { email, password });
+      const res = await axios.post("https://mern-ecommerce-g655.onrender.com/api/v1/auth/login", { email, password });
       if (res && res.data.success) {
         toast.success(res.data.message);
         setAuth({
