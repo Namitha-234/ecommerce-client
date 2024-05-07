77bb9c8266f466e2e738df0ad3e7ca9660c23b28
Namitha-234 <139944475+Namitha-234@users.noreply.github.com>
2024-05-07 11:05:17 +0530

    Update CreateProduct.jsx

--- src/pages/Admin/CreateProduct.jsx
+++ src/pages/Admin/CreateProduct.jsx
@@ -54,7 +54,7 @@ const CreateProduct = () => {
         toast.error(data?.message);
       } else {
         toast.success("Product Created Successfully");
-        navigate("https://mern-ecommerce-g655.onrender.com/dashboard/admin/products");
+        navigate("/dashboard/admin/products");
       }
     } catch (error) {
       console.log(error);
